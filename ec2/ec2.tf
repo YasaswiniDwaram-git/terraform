44e4d681a97e0baffa861ae0a2a084020f5506a0
Satya potti <satyamachani069@gmail.com>
2024-11-19 18:04:05 +0530

    expense

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh_terraform" {
-    name = " allow-shh "
+    name = " allow-SSH "
     description = " Allow port 22"
     
 
@@ -20,4 +20,19 @@ resource "aws_security_group" "allow_ssh_terraform" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  tags = {
+    Name = "allow-ssh"
+  }
+
 }
+
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = {
+    Name = "terraform"
+  }
+
+}
+
